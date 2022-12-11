05d65d91dfedf5dc6cbf00ef7f47bf017dfe9d89
Cyphersnake <cyphersnake@pm.me>
2022-12-11 05:33:33 +0400

    Implement v0.0.0-alpha.0 simple oracle pallet

--- pallets/simple_oracle/src/tests.rs
+++ pallets/simple_oracle/src/tests.rs
@@ -1,23 +1,106 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use crate::{mock::*, Error, Event};
+use frame_support::{assert_noop, assert_ok, error::BadOrigin, pallet_prelude::Get};
+
+const DATA: [u8; 32] = [10; 32];
 
 #[test]
-fn it_works_for_default_value() {
+fn push_data() {
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(SimpleOracleModule::do_something(RuntimeOrigin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(SimpleOracleModule::something(), Some(42));
+		assert_ok!(SimpleOracleModule::push_data(
+			RuntimeOrigin::signed(Test::DEFAULT_ORACLE_ACCOUNT_ID),
+			DATA.into(),
+		));
+
+		let storage = SimpleOracleModule::oracle_data().unwrap();
+		assert_eq!(storage.as_slice(), [DATA.to_vec()]);
+
+		System::assert_last_event(RuntimeEvent::SimpleOracleModule(Event::Emitted {
+			data: DATA.to_vec(),
+		}));
 	});
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn push_data_authority_error() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
+		assert!(SimpleOracleModule::oracle_data().is_none());
+
 		assert_noop!(
-			SimpleOracleModule::cause_error(RuntimeOrigin::signed(1)),
-			Error::<Test>::NoneValue
+			SimpleOracleModule::push_data(RuntimeOrigin::signed(1), DATA.to_vec()),
+			Error::<Test>::WrongAuthority
 		);
+		assert_noop!(
+			SimpleOracleModule::push_data(RuntimeOrigin::none(), DATA.to_vec()),
+			BadOrigin
+		);
+		assert_noop!(
+			SimpleOracleModule::push_data(RuntimeOrigin::root(), DATA.to_vec()),
+			BadOrigin
+		);
+
+		assert!(SimpleOracleModule::oracle_data().is_none());
+	});
+}
+
+#[test]
+fn test_lifetime() {
+	new_test_ext().execute_with(|| {
+		let data_of_moment = |moment: u64| moment.to_be_bytes().to_vec();
+		let lifetime = <Test as crate::Config>::OracleDataLifetime::get();
+		(0..lifetime).for_each(|moment| {
+			Timestamp::set_timestamp(moment);
+			assert_ok!(SimpleOracleModule::push_data(
+				RuntimeOrigin::signed(Test::DEFAULT_ORACLE_ACCOUNT_ID),
+				data_of_moment(moment),
+			));
+		});
+
+		assert_eq!(
+			SimpleOracleModule::oracle_data(),
+			Some((0..lifetime).map(data_of_moment).collect::<Vec<_>>())
+		);
+
+		(lifetime..lifetime * 2).for_each(|moment| {
+			Timestamp::set_timestamp(moment);
+
+			assert_eq!(
+				SimpleOracleModule::oracle_data(),
+				Some((((moment - lifetime) + 1)..lifetime).map(data_of_moment).collect::<Vec<_>>())
+			);
+		});
+
+		assert_eq!(SimpleOracleModule::oracle_data(), Some(vec![]));
+	});
+}
+
+#[test]
+fn test_cleanup() {
+	new_test_ext().execute_with(|| {
+		let data_of_moment = |moment: u64| moment.to_be_bytes().to_vec();
+		let lifetime = <Test as crate::Config>::OracleDataLifetime::get();
+		(0..lifetime).for_each(|moment| {
+			Timestamp::set_timestamp(moment);
+			assert_ok!(SimpleOracleModule::push_data(
+				RuntimeOrigin::signed(Test::DEFAULT_ORACLE_ACCOUNT_ID),
+				data_of_moment(moment),
+			));
+		});
+
+		let data = Some((0..lifetime).map(data_of_moment).collect::<Vec<_>>());
+		assert_eq!(SimpleOracleModule::oracle_data(), data);
+		assert_ok!(SimpleOracleModule::clean_outdated_data(RuntimeOrigin::none(),));
+		assert_eq!(SimpleOracleModule::oracle_data(), data);
+
+		(lifetime..lifetime * 2).for_each(|moment| {
+			Timestamp::set_timestamp(moment);
+			assert_ok!(SimpleOracleModule::clean_outdated_data(RuntimeOrigin::none(),));
+
+			assert_eq!(
+				SimpleOracleModule::oracle_data(),
+				Some((((moment - lifetime) + 1)..lifetime).map(data_of_moment).collect::<Vec<_>>())
+			);
+		});
+
+		assert_eq!(SimpleOracleModule::oracle_data(), Some(vec![]));
 	});
 }
